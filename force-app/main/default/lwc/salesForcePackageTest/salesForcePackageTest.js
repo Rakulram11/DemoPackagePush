d4faa70a43555f324829b88ad7b1c59750ab117b
Rakul Ram <rakulramselvi@gmail.com>
2024-12-17 11:29:52 +0530

    Package test

---
+++ force-app/main/default/lwc/salesForcePackageTest/salesForcePackageTest.js
@@ -0,0 +1,25 @@
+import { LightningElement, wire } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController1.getContacts';
+ 
+export default class ContactDatatable extends LightningElement {
+    columns = [
+        { label: 'First Name', fieldName: 'FirstName', type: 'text' },
+        { label: 'Last Name', fieldName: 'LastName', type: 'text' },
+        { label: 'Email', fieldName: 'Email', type: 'email' },
+        { label: 'Phone', fieldName: 'Phone', type: 'phone' }
+    ];
+    contacts;
+    error;
+ 
+    @wire(getContacts)
+    wiredContacts({ error, data }) {
+        if (data) {
+            this.contacts = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.contacts = undefined;
+        }
+    }
+}
+ 
